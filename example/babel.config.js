9d1576511eb0ad9875cc4dad64e3be831ef92153
Tuan Luong <tuanluong.it@gmail.com>
2020-10-10 15:00:36 +0700

    update to reanimated 2

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // NOTE: Reanimated plugin has to be listed last.
+    'react-native-reanimated/plugin',
+  ],
 };
